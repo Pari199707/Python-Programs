cf48d7bf06a2cb4d28acb5c33fe5a189f5582099
Pari199707 <121976856+Pari199707@users.noreply.github.com>
2023-08-12 00:29:08 +0530

    Add files via upload

---
+++ Sorted and Reversed functions.py
@@ -0,0 +1,8 @@
+list1=[12,43,10,87,45]
+print(list1)
+sorted_list=sorted(list1)
+print(sorted_list)
+sorted_reverse_list=sorted(list1,reverse=True)
+print(sorted_reverse_list)
+reversed_list=reversed(list1)
+print(list(reversed_list)) 
